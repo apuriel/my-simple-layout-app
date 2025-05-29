67bbc999efc1528da69a597d0535f6471461a447
Ariel Colombo <apuriel@msn.com>
2025-05-29 18:44:58 -0300

    upd

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,103 +1,109 @@
-import { useState } from 'react';
+import { useState } from "react";
 import styles from "./Sidebar.module.css";
 
 export interface MenuItem {
   id: number;
   title: string;
-  isActive?: boolean;
+//   isActive?: boolean;
   icon?: string;
   path?: string;
   children?: MenuItem[];
 }
 
+interface menuStateProps {
+    activeId:   number  | null;
+    openId:     Set<number>;
+}
+
 const sideMenu: MenuItem[] = [
-    { id: 0, title: 'Dashboard'},
-    { id: 1, title: 'Ventas',    children:
-        [
-            {id: 2, title: 'Presupuestos'},
-            {id: 3, title: 'Pedidos'},
-            {id: 4, title: 'Facturas'},
-            {id: 5, title: 'Reportes'}, 
-        ]},
-    { id: 6, title: 'Compras'},
-    { id: 7, title: 'Clientes'},
-    { id: 8, title: 'RRHH'},
-    { id: 9, title: 'Bancos'},
-    { id: 10, title: 'Cuentas'},
-    { id: 11, title: 'Inventario'},
+  { id: 0, title: "Dashboard" },
+  {
+    id: 1,
+    title: "Ventas",
+    children: [
+      { id: 2, title: "Presupuestos" },
+      { id: 3, title: "Pedidos" },
+      { id: 4, title: "Facturas" },
+      { id: 5, title: "Reportes" },
+    ],
+  },
+  { id: 6, title: "Compras" },
+  { id: 7, title: "Clientes" },
+  { id: 8, title: "RRHH" },
+  { id: 9, title: "Bancos" },
+  { id: 10, title: "Cuentas" },
+  { id: 11, title: "Inventario" },
 ];
 
-export function Sidebar()
-{
-    const [activeId, setActiveId] = useState<number | null>(null);
-    const [expandedIds, setExpandedIds] = useState<Set<number>>(new Set());
+// export function Sidebar() {
+//   const [activeId, setActiveId] = useState<number | null>(null);
 
-    const toggleExpand = (id: number) => {
-        setExpandedIds(prev => {
-            const newSet = new Set(prev);
-            if (newSet.has(id)) newSet.delete(id);
-            else newSet.add(id);
-            return newSet;
-        });
-    };
+//   const renderMenuItems = (list: MenuItem[]) => (
+//     <ul>
+//       {list.map(({ id, title, children }) => {
+//         const isActive = activeId === id;
 
-    const handleClick = (id: number, hasChildren: boolean) => {
-        if (hasChildren) {
-            toggleExpand(id);
-        } else {
-            setActiveId(id);
-        }
-    };
+//         return (
+//         <li key={id}
+//             className= {   `${styles.menuItemWrapper} 
+//                             ${children ? styles.hasChildren : ""} 
+//                             ${isActive ? styles.active : ""} 
+//                             ${isExpanded ? styles.expanded : ""}`
+//                         }>
+//             <a href="#" onClick={(e) => { e.preventDefault(); handleClick(id, !!children); }} className={isActive ? styles.active : ""}>
+//                 <span className={styles.itemDescription}>{title}</span>
+//             </a>
 
-    const renderMenuItems = (list: MenuItem[]) => (
-        <ul>
-            {list.map(({ id, title, children }) => {
-                const isExpanded = expandedIds.has(id);
-                const isActive = activeId === id;
+//             {children && (<ul className={isExpanded ? styles.submenuOpen : styles.submenuClosed}> {renderMenuItems(children)}</ul>)}
+//         </li>
+//         );
+//       })}
+//     </ul>
+//   );
 
-                return (
-                    <li 
-                        key={id} 
-                        className={`
-                            ${styles.menuItemWrapper} 
-                            ${children ? styles.hasChildren : ''} 
-                            ${isActive ? styles.active : ''} 
-                            ${isExpanded ? styles.expanded : ''}`}
-                    >
-                        <a
-                            href="#"
-                            onClick={(e) => {
-                                e.preventDefault();
-                                handleClick(id, !!children);
-                            }}
-                            className={isActive ? styles.active : ''}
-                        >
-                            <span className={styles.itemDescription}>{title}</span>
-                        </a>
+  export function Sidebar() {
 
-                        {children && (
-                        <ul className={isExpanded ? styles.submenuOpen : styles.submenuClosed}>
-                            {renderMenuItems(children)}
-                        </ul>
-)}
+  const [menuState, setMenuState] = useState<menuStateProps>({activeId: null, openId: new Set()});
 
-                    </li>
-                );
-            })}
-        </ul>
-    );
+  const handleClick = (id: number, hasChildren: boolean) => {
+    setMenuState(prev => ({
+        ...prev,
+        activeId: id
+    }));
+  };
 
-    return ( 
-       <aside className={styles.appSidebar}>
-            <div className={styles.sidebarHeader}>
-                <h3>ERP-PRO</h3>
-            </div>
-            <nav>
-                {renderMenuItems(sideMenu)}
-            </nav>
-            <div className={styles.sidebarFooter}>
-                <span>v1.0</span>
-            </div>
-        </aside>
-    );
+  const renderMenuItems = (list: MenuItem[]) => (
+    <ul>
+      {list.map(({ id, title, children }) => {
+
+        return (
+        <li key={id}>
+            <a href="#" onClick={(e) => { e.preventDefault(); handleClick(id, !!children); }} className={(menuState.activeId == id) ? styles.active : ""}>
+                <span className={styles.itemDescription}>{title}</span>
+            </a>
+          
+        </li>
+        );
+      })}
+    </ul>
+  );
+
+  return (
+    <aside className={styles.appSidebar}>
+      <div className={styles.sidebarHeader}>
+        <h3>ERP-PRO</h3>
+      </div>
+      <nav>{renderMenuItems(sideMenu)}</nav>
+      <div className={styles.sidebarFooter}>
+        <span>v1.0</span>
+      </div>
+    </aside>
+  );
 }
+// Si tiene childrens no es seleccionable,
+// Pero tiene q poner la clase expandir y crear una ul nueva y llamar al render nuevamente
+// este elemento con childrens si se ya se encuentra expandido entonces hayq ue contraerlo
+// dentro de cualquier menu sin children hay q marcar si es activo o no.
+
+// puedo crearme un array usarlo de buffer
+
